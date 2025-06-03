af731ecd3dea80621c5e9b52f74c757f6715d613
KidusDZ <114849668+KidusDZ@users.noreply.github.com>
2025-06-03 23:56:27 +0300

    Redesign (#11)
    * upddate
    
    * Login Update
    
    * Update
    
    * Update vehicle and user managment
    
    * M Update
    
    * new
    
    * update m
    
    * update
    
    * Update VehicleApprovalModal.jsx
    
    * Update Login.jsx

--- src/newPages/Login.jsx
+++ src/newPages/Login.jsx
@@ -1,22 +1,34 @@
-import { TextField, OutlinedInput, InputAdornment, IconButton, FormControl, InputLabel, Button, Typography, Box } from "@mui/material";
-import { styled } from '@mui/system';
+
+import {
+  TextField,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  FormControl,
+  InputLabel,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
+import { styled } from "@mui/system";
 
 import myImage from "../assets/myImage.png";
-import React, { useEffect, useState } from 'react';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import React, { useEffect, useState } from "react";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../store/auth/authThunks";
 
 // Styled Box component for consistent styling
 const StyledBox = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '100%',
-  width: '100%',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+  width: "100%",
 }));
 
 function Login() {
@@ -25,54 +37,57 @@ function Login() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate(); // Initialize useNavigate
   const dispatch = useDispatch();
-  const { loading, error, isAuthenticated } = useSelector((state) => state.auth);
+  const { loading, error, isAuthenticated } = useSelector(
+    (state) => state.auth
+  );
   const [errors, setErrors] = useState({});
   const [loginError, setLoginError] = useState("");
 
   useEffect(() => {
-   if (isAuthenticated) {
-    navigate("/dashboard");
-    setTimeout(() => {
-         window.location.reload();
-       }, 100);
-   }
-  }, [isAuthenticated, navigate]);
- 
-  const handleSubmit = async (e) => {
-   e.preventDefault();
-   setLoginError(""); // Clear previous login error
-   setErrors({}); // Clear previous validation errors
- 
-   // Basic client-side validation
-   const validationErrors = {};
-   if (!phone_number) {
-    validationErrors.phone_number = "Phone number is required";
-   }
-   if (!password) {
-    validationErrors.password = "Password is required";
-   }
-   setErrors(validationErrors);
- 
-   if (Object.keys(validationErrors).length > 0) {
-    return;
-   }
- 
-   try {
-    const resultAction = await dispatch(login(phone_number, password)); // Corrected dispatch syntax
-    const result = await resultAction.unwrap();
- 
-    console.log("Login successful:", result);
- 
-   } catch (rejectedValueOrSerializedError) {
-    console.error("Login failed:", rejectedValueOrSerializedError);
-    if (rejectedValueOrSerializedError) {
-     setLoginError("Phone number or password is incorrect. Please try again.");
-    } else {
-     setLoginError("An error occurred during login. Please try again.");
+    if (isAuthenticated) {
+      navigate("/dashboard");
+      setTimeout(() => {
+        window.location.reload();
+      }, 100);
     }
-   }
+  }, [isAuthenticated, navigate]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoginError(""); // Clear previous login error
+    setErrors({}); // Clear previous validation errors
+
+    // Basic client-side validation
+    const validationErrors = {};
+    if (!phone_number) {
+      validationErrors.phone_number = "Phone number is required";
+    }
+    if (!password) {
+      validationErrors.password = "Password is required";
+    }
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length > 0) {
+      return;
+    }
+
+    try {
+      const resultAction = await dispatch(login(phone_number, password)); // Corrected dispatch syntax
+      const result = await resultAction.unwrap();
+
+      console.log("Login successful:", result);
+    } catch (rejectedValueOrSerializedError) {
+      console.error("Login failed:", rejectedValueOrSerializedError);
+      if (rejectedValueOrSerializedError) {
+        setLoginError(
+          "Phone number or password is incorrect. Please try again."
+        );
+      } else {
+        setLoginError("An error occurred during login. Please try again.");
+      }
+    }
   };
- 
+
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleMouseDownPassword = (event) => {
@@ -83,27 +98,66 @@ function Login() {
     event.preventDefault();
   };
 
-  
   return (
     <>
       {/* whole page */}
-      <Box sx={{ display: 'flex', height: '100vh', width: '100vw' }}>
+      <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
         {/* left */}
-        <StyledBox sx={{ width: '50%', backgroundColor: '#e0f2f7' }}> 
-          <Box sx={{ textAlign: 'center' }}>
-            <Typography variant="h4" sx={{ whiteSpace: 'nowrap', fontWeight: 'extrabold' }}>Welcome to Guzo Rentals</Typography> 
+        <StyledBox sx={{ width: "50%", backgroundColor: "#e0f2f7" }}>
+          <Box sx={{ textAlign: "center" }}>
+            <Typography
+              variant="h4"
+              sx={{ whiteSpace: "nowrap", fontWeight: "extrabold" }}
+            >
+              Welcome to Guzo Rentals
+            </Typography>
           </Box>
-          <Box sx={{ px: 16, mt: 4, textAlign: 'center', overflowWrap: 'break-word', flexDirection: 'row', flexWrap: 'wrap' }}> 
-            <Typography variant="body2" sx={{ color: 'rgba(0, 0, 0, 0.87)' }}>Well, let me tell you something, funny boy. Y'know that little stamp, the one that says "New York Public Library"? Well that may not mean anything to you, but that means a lot to me. One whole hell of a lot.</Typography> 
+          <Box
+            sx={{
+              px: 16,
+              mt: 4,
+              textAlign: "center",
+              overflowWrap: "break-word",
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            <Typography variant="body2" sx={{ color: "rgba(0, 0, 0, 0.87)" }}>
+              Welcome to the admin page.
+            </Typography>
           </Box>
         </StyledBox>
         {/* right */}
-        <Box sx={{ width: '50%', height: '100vh', backgroundImage: `url(${myImage})`, backgroundSize: 'contain', backgroundPosition: 'center', backgroundColor: '#0d47a1',display: 'flex', justifyContent: 'center', alignItems: 'center' }}> 
+        <Box
+          sx={{
+            width: "50%",
+            height: "100vh",
+            backgroundImage: `url(${myImage})`,
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundColor: "#0d47a1",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           {/* center */}
-          <StyledBox sx={{  width: 300, height: 320, gap: 5, borderRadius: '8px', opacity: 0.85, backgroundColor: 'white', p: 3 }}> 
+          <StyledBox
+            sx={{
+              width: 300,
+              height: 320,
+              gap: 5,
+              borderRadius: "8px",
+              opacity: 0.85,
+              backgroundColor: "white",
+              p: 3,
+            }}
+          >
             {/* form */}
-            <Box sx={{ width: '100%' }}> 
-              <Typography variant="h4" align="left" sx={{ pb: 6, pr: 12 }}>Login</Typography> 
+            <Box sx={{ width: "100%" }}>
+              <Typography variant="h4" align="left" sx={{ pb: 6, pr: 12 }}>
+                Login
+              </Typography>
               <TextField
                 className="outlined-basic text-xs font-extralight "
                 size="small"
@@ -111,19 +165,30 @@ function Login() {
                 variant="outlined"
                 value={phone_number}
                 onChange={(e) => setphoneNumber(e.target.value)}
-                sx={{ fontSize: '0.75rem', fontWeight: 300 }} 
+                sx={{ fontSize: "0.75rem", fontWeight: 300 }}
               />
             </Box>
-            <Box sx={{ width: '100%' }}> 
+            <Box sx={{ width: "100%" }}>
               <FormControl variant="outlined" size="small">
-                <InputLabel htmlFor="outlined-adornment-password"  variant="outlined" sx={{ fontSize: '0.95rem', fontWeight: 300 }}>Password</InputLabel> {/* Approximate of outlined-basic text-xs font-extralight */}
-                <OutlinedInput    
+                <InputLabel
+                  htmlFor="outlined-adornment-password"
+                  variant="outlined"
+                  sx={{ fontSize: "0.95rem", fontWeight: 300 }}
+                >
+                  Password
+                </InputLabel>{" "}
+                {/* Approximate of outlined-basic text-xs font-extralight */}
+                <OutlinedInput
                   id="outlined-adornment-password"
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   endAdornment={
                     <InputAdornment position="start">
                       <IconButton
-                        aria-label={showPassword ? 'hide the password' : 'display the password'}
+                        aria-label={
+                          showPassword
+                            ? "hide the password"
+                            : "display the password"
+                        }
                         onClick={handleClickShowPassword}
                         onMouseDown={handleMouseDownPassword}
                         onMouseUp={handleMouseUpPassword}
@@ -136,15 +201,40 @@ function Login() {
                   label="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  sx={{ fontSize: '0.75rem', fontWeight: 300 }} 
+                  sx={{ fontSize: "0.75rem", fontWeight: 300 }}
                 />
               </FormControl>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2, mb: 2 }}> 
-                <Typography variant="caption" sx={{ textDecoration: 'underline', fontSize: '0.75rem' }}> 
-                  <a href="#" style={{ color: 'inherit', textDecoration: 'inherit' }}>Forgot Password?</a>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  mt: 2,
+                  mb: 2,
+                }}
+              >
+                <Typography
+                  variant="caption"
+                  sx={{ textDecoration: "underline", fontSize: "0.75rem" }}
+                >
+                  <a
+                    href="#"
+                    style={{ color: "inherit", textDecoration: "inherit" }}
+                  >
+                    Forgot Password?
+                  </a>
                 </Typography>
               </Box>
-              <Button variant="contained" sx={{ backgroundColor: '#000080', borderRadius: 10, width: '100%' }} size="small" onClick={handleSubmit}  >
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#000080",
+                  borderRadius: 10,
+                  width: "100%",
+                }}
+                size="small"
+                onClick={handleSubmit}
+              >
+
                 Login
               </Button>
             </Box>
@@ -156,3 +246,4 @@ function Login() {
 }
 
 export default Login;
+
